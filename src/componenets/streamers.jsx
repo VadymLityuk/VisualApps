717d4da4c8e3edac671662de5cbbb367e4717c6a
Vadym Lityuk <57510267+VadymLityuk@users.noreply.github.com>
2023-01-29 11:16:31 -0500

    Update streamers.jsx

--- src/componenets/streamers.jsx
+++ src/componenets/streamers.jsx
@@ -1,54 +1,51 @@
-import React, { useEffect,useState }  from 'react';
-import moment from 'moment'
+import React,{useEffect,useState} from 'react'
 import api from './twitch-api'
+import moment from 'moment'
 
-export function Streamers() {
-  const [streams, setStreams] = useState([]);
 
- 
-useEffect(() => {
-  // declare the data fetching function
-  const fetchData = async () => {
-    const result = await api.get("https://api.twitch.tv/helix/streams", { params: { first: 50 } })
+export function Streamers(){
+ const[streams,setStreams] = useState([])
+
+ useEffect(()=>{
+    // here we gonna declare the data fetching function 
+    const fetchData = async()=>{
+    const result = await api.get("https://api.twitch.tv/helix/streams",{params:{first: 50}})
     const streamsData = [];
-    if(result){
-      const firstThirty = result?.data?.data?.slice(0,30) //take first 30 streams, because twitch return not exact number of streams
-      firstThirty?.forEach(x=>
-        {
-          const streamStartTimeFormat = moment(x.started_at)
-          streamsData.push({
-            userName: x.user_name,
-            viewersNumber: x.viewer_count,
-            game: x.game_name,
-            startTime: streamStartTimeFormat.format('LLL'),
-            streamDuration: streamStartTimeFormat.fromNow()
-          })
-        }
-      )
-    }
-    console.log(streamsData)
-    setStreams(streamsData)
-  }
+    const firstThirty = result?.data.data?.slice(0,30)// this will allow us to take first 30 streams because twitch data show always one more
+          firstThirty?.forEach(x=>{
+            const streamStartTime = moment(x.started_at) 
+            streamsData.push({
+                userName: x.user_name,
+                viewersNumber: x.viewer_count,
+                game: x.game_name,
+                startTime: streamStartTime.format('LLL'),
+                streamDuration: streamStartTime.fromNow()
+            })
+         })
+   console.log(streamsData)
+   setStreams(streamsData)
+}
+fetchData()
+ },[])
 
-  fetchData()
-}, [])
-
-
-      return (
+    return(
         <div>
-          <div className="App">
-        <div className="App-header">
-        {streams && streams?.length > 0 ? streams.map( (stream, index) => ( 
-        <div className="StylerCard">
-          <div>Stream #{index+1}:<p style={{fontWeight: 'bold'}}>{stream.userName} </p>now streaming {stream.game}</div>
-          <div>Viewers: {stream.viewersNumber}</div>
-          <div>Started {stream.startTime} {stream.streamDuration}</div>
+            <div className="App">
+                <div className="App-header">
+                    {streams && streams.length> 0 ?   streams.map((value,index)=>
+                    <div className="streamersCard">
+                    <h1>{value.userName}</h1>
+                    <p>Viewers: {value.viewersNumber}</p>
+                    <p>Game:{value.game}</p>
+                    <p>Started:{value.startTime},{value.streamDuration}</p>
+                     </div>) :
+                        <div className="streamersCard">
+                        <h1>No Streams Found</h1>
+                        </div>
+                     }
+                </div>
+            </div>
         </div>
-        )):
-        <div>No streams found</div>
-        }  
-        </div>
-      </div>
-        </div>
-      );
-  }
+    )
+}
+
